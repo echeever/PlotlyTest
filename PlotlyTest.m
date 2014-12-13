fbd974df693e0f82d3174a3ab1a0322efea83b28
Erik.A.Cheever <erik_cheever@swarthmore.edu>
2014-12-12 21:31:27 -0500

    First Matlab Commit

--- PlotlyTest.m
+++ PlotlyTest.m
@@ -1,3 +1,4 @@
 [X,Y,Z] = peaks;
 contour(X,Y,Z,20);
-res=fig2plotly(gcf,'filename','ThisIsThis','fileopt','overwrite','link','true')
+res=fig2plotly(gcf,...
+    'filename','ThisIsThis','fileopt','overwrite','link','true')
