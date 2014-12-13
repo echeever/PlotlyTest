abb8794035a2bd57d24bd65d5ad6298e07d35a6a
Erik.A.Cheever <erik_cheever@swarthmore.edu>
2014-12-12 22:08:43 -0500

    Second commit using matlab

--- PlotlyTest.m
+++ PlotlyTest.m
@@ -1,4 +1,16 @@
-[X,Y,Z] = peaks;
-contour(X,Y,Z,20);
+
+y = magic(4);
+positionVector1 = [0.1, 0.2, 0.3, 0.3];
+subplot('Position',positionVector1)
+
+plot(y)
+legend({'a' 'b' 'c' 'd'});
+
+
+
+positionVector2 = [0.5, 0.1, 0.4, 0.7];
+subplot('Position',positionVector2)
+bar(y)
+
 res=fig2plotly(gcf,...
-    'filename','ThisIsThis','fileopt','overwrite','link','true')
+    'filename','ThisIsThis','fileopt','overwrite')
